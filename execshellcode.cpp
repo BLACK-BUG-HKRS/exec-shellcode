c84f780bc40327f1a70ac2145dd86f0e3b2a23c8
Major2000 <majormadobe@gmail.com>
2022-09-27 14:35:11 +0300

    Allocate memory in a specific region

---
+++ execshellcode.cpp
@@ -0,0 +1,13 @@
+#include <windows.h> // header file for windows API
+#include <stdio.h>
+
+int main()
+{
+    char shellcode[] = {};
+    int shellcode_size = sizeof(shellcode);
+    void *exec_mem; // allocated Memory
+
+    exec_mem = VirtualAlloc(NULL, shellcode_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE); // Allocate memory in a specific region 
+    
+
+}
