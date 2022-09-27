c38b22770f3f066e7e3e1b8640b3a161342e432b
Major2000 <majormadobe@gmail.com>
2022-09-27 14:37:23 +0300

    move the shellcode to the allocated memory region

--- execshellcode.cpp
+++ execshellcode.cpp
@@ -8,6 +8,7 @@ int main()
     void *exec_mem; // allocated Memory
 
     exec_mem = VirtualAlloc(NULL, shellcode_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE); // Allocate memory in a specific region 
+    RtlMoveMemory(exec_mem, shellcode, shellcode_size); // move the shellcode to the allocated memory region
     
 
 }
